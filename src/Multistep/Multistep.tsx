46ef035c13be9a49801171babb6cd5f2d7da6cb5
Talha Ahmed <49815075+TalhaAhmedAI@users.noreply.github.com>
2021-01-13 03:33:54 -0500

    Mutistep form completed

--- src/Multistep/Multistep.tsx
+++ src/Multistep/Multistep.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
-import {Card, CardContent, Button} from '@material-ui/core'
+import {Card, CardContent, Button, Box, Step, StepLabel, Stepper, Grid, CircularProgress} from '@material-ui/core'
 import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik'
 import { CheckboxWithLabel, TextField } from 'formik-material-ui'
 
 import MultistepSchema from './Schema'
+
+const sleep = (time: number) => new Promise((acc) => setTimeout(acc, time))
 
 export function Multistep() {
     return (
@@ -16,17 +18,30 @@ export function Multistep() {
                     millionare: false,
                     money: 0,
                     description: ''
-                }} onSubmit={() => {}}>
-                        <FormikStep>
-                        <Field name="firstName" component={TextField} label="First Name" />
-                        <Field name="lastName" component={TextField} label="Last Name" />
-                        <Field name="millionare" type="checkbox" component={CheckboxWithLabel} Label={{ label: "I'm a millionare"}} />
+                }} onSubmit={async (values) => {
+                    await sleep(3000)
+                    console.log("values", values)
+                }}>
+                        <FormikStep label="Personal Data">
+                        <Box paddingBottom={2}>
+                        <Field fullWidth name="firstName" component={TextField} label="First Name" />
+                        </Box>
+                        <Box paddingBottom={2}>
+                        <Field fullWidth name="lastName" component={TextField} label="Last Name" />
+                        </Box>
+                        <Box paddingBottom={2}>
+                        <Field fullWidth name="millionare" type="checkbox" component={CheckboxWithLabel} Label={{ label: "I'm a millionare"}} />
+                        </Box>
                         </FormikStep>
-                        <FormikStep validationSchema={MultistepSchema}>
-                        <Field name="money" type="number" component={TextField} label="Money" />
+                        <FormikStep label="Bank Information" validationSchema={MultistepSchema}>
+                        <Box paddingBottom={2}>
+                        <Field fullWidth name="money" type="number" component={TextField} label="Money" />
+                        </Box>
                         </FormikStep>
-                        <FormikStep>
-                        <Field name="description" component={TextField} label="Description" />
+                        <FormikStep label="More Info">
+                        <Box paddingBottom={2}>
+                        <Field fullWidth name="description" component={TextField} label="Description" />
+                        </Box>
                         </FormikStep>
                 </FormikStepper>
             </CardContent>
@@ -35,7 +50,7 @@ export function Multistep() {
 }
 
 export interface FormikStepProps extends Pick<FormikConfig<FormikValues>, 'children' | 'validationSchema'> {
-
+    label: string
 }
 
 export function FormikStep({ children }: FormikStepProps) {
@@ -44,28 +59,44 @@ export function FormikStep({ children }: FormikStepProps) {
 
 export function FormikStepper({ children, ...props}: FormikConfig<FormikValues>) {
 
-    const childrenArray = React.Children.toArray(children)
+    const childrenArray = React.Children.toArray(children) as React.ReactElement<FormikStepProps>[]
     const [step, setStep] = useState(0)
     const currentChild = childrenArray[step]
-    console.log('children', currentChild)
+    const [completed, setCompleted] = useState(false)
 
     function isLastStep() {
         return step === childrenArray.length - 1
     }
 
     return (
-        <Formik {...props} onSubmit={async (values, helpers) => {
+        <Formik 
+        {...props}
+        validationSchema={currentChild.props.validationSchema}
+        onSubmit={async (values, helpers) => {
             if (isLastStep()) {
                 await props.onSubmit(values, helpers)
+                setCompleted(true)
             } else {
                 setStep(s => s + 1)
             }
         }}>
-            <Form autoComplete="off">
+            {({isSubmitting}) => (
+
+                <Form autoComplete="off">
+            <Stepper alternativeLabel activeStep={step}>
+            {childrenArray.map((child, index) => (
+                <Step key={child.props.label} completed={step > index || completed}>
+                <StepLabel>{child.props.label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
                 {currentChild}
-                {step > 0 ? <Button onClick={() => {}}>Back</Button> : null}
-                <Button type="submit">{isLastStep() ? 'Submit' : 'Next'}</Button>
+                <Grid container spacing={2}>
+                {step > 0 ? <Grid item> <Button disabled={isSubmitting} color="primary" variant="contained" onClick={() => {}}>Back</Button></Grid> : null}
+                <Grid item><Button startIcon={isSubmitting ? <CircularProgress size="1rem" /> :null } disabled={isSubmitting} color="primary" variant="contained" type="submit">{isSubmitting ? 'Submitting' : isLastStep() ? 'Submit' : 'Next'}</Button></Grid>
+                </Grid>
                 </Form>
+            )}
         </Formik>
     )
 }
